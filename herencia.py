d498743d41bd18835b5ef2963b6c8113c655d82c
matez204 <mzclavijor1204@gmail.com>
2024-02-27 11:47:29 -0500

    Herencia

---
+++ herencia.py
@@ -0,0 +1,32 @@
+class Persona:
+    def __init__(self,nombre,edad,nacionalidad):
+        self.nombre = nombre
+        self.edad = edad
+        self.nacionalidad = nacionalidad
+    def hablar(self):
+        print("Hablando...")
+
+class Artista:
+    def __init__(self,habilidad):
+        self.habilidad=habilidad
+    def mostrar_habilidad(self):
+        print(f"Mi habilidad es {self.habilidad}")
+        
+class Empleado(Persona):
+    def __init__(self,nombre,edad,nacionalidad,trabajo,salario):
+        super().__init__(nombre, edad, nacionalidad)
+        self.trabajo=trabajo
+        self.salario=salario
+
+class EmpleadoArtista(Persona,Artista):
+    def __init__(self,nombre,edad,nacionalidad,habilidad,salario,empresa):
+        Persona.__init__(self,nombre,edad,nacionalidad)
+        Artista.__init__(self,habilidad)
+        self.salario=salario
+        self.empresa=empresa
+
+        
+cosa=EmpleadoArtista("Roberto", 22,"colombiano","cocinar",2000,"Columbia")
+
+print(issubclass(EmpleadoArtista,Artista))#Nos dice si es una subclase de alguien
+print(isinstance(cosa, Artista))#Nos dice si es un obojeto de esa clase 
